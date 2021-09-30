4181b735838114f3b64d6aac7d58fc08f7eb2c6e
Changhui Xu <changhuixu@hotmail.com>
2021-09-30 15:33:59 -0500

    consolidate css in footer 3

--- src/app/uids-footer/uids-footer.component.ts
+++ src/app/uids-footer/uids-footer.component.ts
@@ -3,10 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'uids-footer',
   templateUrl: './uids-footer.component.html',
-  styleUrls: ['./uids-footer.component.css', '../uids-iowa-bar/logo.css'],
+  styleUrls: [
+    './uids-footer.component.css',
+    '../uids-iowa-bar/logo.css',
+    '../flex.css',
+  ],
 })
 export class UidsFooterComponent implements OnInit {
+  year = 2021;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const today = new Date();
+    this.year = today.getFullYear();
+  }
 }
--- src/app/uids-iowa-bar/uids-iowa-bar.component.ts
+++ src/app/uids-iowa-bar/uids-iowa-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'uids-iowa-bar',
   templateUrl: './uids-iowa-bar.component.html',
-  styleUrls: ['logo.css', 'iowa-bar.css', 'site-name.css'],
+  styleUrls: ['logo.css', 'iowa-bar.css', 'site-name.css', '../flex.css'],
 })
 export class UidsIowaBarComponent implements OnInit {
   constructor() {}
